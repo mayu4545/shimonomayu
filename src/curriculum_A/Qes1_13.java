58f9888133c3dc0eddf1c7e69fd5483066cf73ee
mumum <mumum@192.168.1.96>
2025-07-07 22:59:41 +0900

    初回コミット

---
+++ src/curriculum_A/Qes1_13.java
@@ -0,0 +1,15 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// 1 ローカル変数として宣言
+		byte byteNum;
+		// 2 ローカル変数を初期値で初期化
+		byteNum = 0;
+		// 3 変数に下記の値を代入
+		byteNum = 10;
+
+	}
+
+}
